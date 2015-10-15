94e79a22db1ad7bb7b027b751659285061b9b60c
rosannawubs <rosannawubs@gmail.com>
2015-10-15 15:00:01 -0700

    Made changes to the Order page so that it displays when you choose "new order". Finished up but not including page 25.

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -17,8 +17,15 @@ class Order extends Application {
 
     // start a new order
     function neworder() {
-        //FIXME
-
+        $order_num = $this->orders->highest() + 1;
+        
+        $neworder = $this->orders->create();
+        $neworder->num = $order_num;
+        $neworder->date = date();
+        $neworder->status = 'a';
+        $neworder->total = 0;
+        
+        $this->orders->add($neworder);
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -27,9 +34,12 @@ class Order extends Application {
         if ($order_num == null)
             redirect('/order/neworder');
 
+        $order = $this->orders->get($order_num);
+        
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        
+        $this->data['title'] = "Order # " .$order_num . ' (' . number_format($this->orders->total($order_num), 2) . ')';
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -63,8 +73,7 @@ class Order extends Application {
     
     // make a menu ordering column
     function make_column($category) {
-        //FIXME
-        return $items;
+        return $this->menu->some('category', $category);
     }
 
     // add an item to an order
